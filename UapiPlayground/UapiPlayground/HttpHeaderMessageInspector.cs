12b4c790d1b4e1d5a37d6bb56205c09066bfa712
Mari van Loggerenberg <mari.vanloggerenberg@gmail.com>
2019-06-04 09:50:48 +0200

    Add project files.

---
+++ UapiPlayground/UapiPlayground/HttpHeaderMessageInspector.cs
@@ -0,0 +1,95 @@
+﻿namespace UapiPlayground
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.ServiceModel;
+    using System.ServiceModel.Channels;
+    using System.ServiceModel.Description;
+    using System.ServiceModel.Dispatcher;
+    using System.Text;
+
+    public class HttpHeaderMessageInspector : IClientMessageInspector
+    {
+        private readonly Dictionary<string, string> _httpHeaders;
+
+        public HttpHeaderMessageInspector(Dictionary<string, string> httpHeaders)
+        {
+            _httpHeaders = httpHeaders;
+        }
+
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+        }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            HttpRequestMessageProperty httpRequestMessage;
+            object httpRequestMessageObject;
+            var userName = "";
+            var passWord = "";
+
+            if (request.Properties.TryGetValue(HttpRequestMessageProperty.Name, out httpRequestMessageObject))
+            {
+                httpRequestMessage = httpRequestMessageObject as HttpRequestMessageProperty;
+                foreach (var httpHeader in _httpHeaders)
+                {
+                    if (string.Compare(httpHeader.Key, "Username", StringComparison.Ordinal) == 0)
+                    {
+                        userName = httpHeader.Value;
+                    }
+                    else if (string.Compare(httpHeader.Key, "Password", StringComparison.Ordinal) == 0)
+                    {
+                        passWord = httpHeader.Value;
+                    }
+                }
+                if (httpRequestMessage != null)
+                    httpRequestMessage.Headers[HttpRequestHeader.Authorization] = "Basic " +
+                                                                                  Convert.ToBase64String(
+                                                                                      Encoding.ASCII.GetBytes(userName +
+                                                                                                              ":" +
+                                                                                                              passWord));
+            }
+            else
+            {
+                httpRequestMessage = new HttpRequestMessageProperty();
+
+                foreach (var httpHeader in _httpHeaders)
+                {
+                    httpRequestMessage.Headers.Add(httpHeader.Key, httpHeader.Value);
+                }
+                request.Properties.Add(HttpRequestMessageProperty.Name, httpRequestMessage);
+            }
+            return null;
+        }
+    }
+
+    internal class HttpHeadersEndpointBehavior : IEndpointBehavior
+    {
+        private readonly Dictionary<string, string> _httpHeaders;
+
+        public HttpHeadersEndpointBehavior(Dictionary<string, string> httpHeaders)
+        {
+            _httpHeaders = httpHeaders;
+        }
+
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            var inspector = new HttpHeaderMessageInspector(_httpHeaders);
+
+            clientRuntime.ClientMessageInspectors.Add(inspector);
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+        }
+
+        public void Validate(ServiceEndpoint endpoint)
+        {
+        }
+    }
+}
---
+++ UapiPlayground/UapiPlayground/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.ServiceModel;
+
+namespace UapiPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AirLowfareShop();
+        }
+
+        private static void AirLowfareShop()
+        {
+            #region MAKE WEB SERVICE CALL TO UAPI
+
+            //THIS ALWAYS REMAINS AS IS - STANDARD
+            var binding = new BasicHttpBinding
+            {
+                Name = "Mari",
+                MaxReceivedMessageSize = 2097152
+            };
+            binding.Security.Mode = BasicHttpSecurityMode.Transport;
+            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
+
+            #endregion
+
+            //THIS ALWAYS REMAINS AS IS - STANDARD
+            var credHeaders = new Dictionary<string, string> { { "Username", "Universal API/uAPI3454945120-33f61ac4" }, { "Password", "9i!N=Ft7Pd" } };
+
+            var client = new SVCuapiAIR.AirLowFareSearchPortTypeClient(binding, new EndpointAddress("https://emea.universal-api.travelport.com/B2BGateway/connect/uAPI/AirService"));    //SERVER WE ARE CONNECTING TO
+
+
+            if (client.ClientCredentials != null)
+            {
+                client.ClientCredentials.UserName.UserName = "Universal API/uAPI3454945120-33f61ac4";
+                client.ClientCredentials.UserName.Password = "9i!N=Ft7Pd";
+            }
+
+            client.Endpoint.EndpointBehaviors.Add(new HttpHeadersEndpointBehavior(credHeaders));
+            ServicePoint servicePoint = ServicePointManager.FindServicePoint(client.Endpoint.Address.Uri, null);
+            servicePoint.Expect100Continue = false;
+
+            client.Open();
+            client.service(null, new SVCuapiAIR.LowFareSearchReq
+            {
+                BillingPointOfSaleInfo = new SVCuapiAIR.BillingPointOfSaleInfo
+                {
+                    OriginApplication = "UAPI"
+                },
+                TargetBranch = "P2771968",
+                Items = new SVCuapiAIR.SearchAirLeg[1]
+                {
+                    new SVCuapiAIR.SearchAirLeg
+                    {
+                        SearchOrigin = new SVCuapiAIR.typeSearchLocation[1]
+                        {
+                            new SVCuapiAIR.typeSearchLocation
+                            {
+                                Item = new SVCuapiAIR.Airport
+                                {
+                                    Code = "JNB"
+                                }
+                            }
+                        },
+                        SearchDestination = new SVCuapiAIR.typeSearchLocation[1]
+                        {
+                            new SVCuapiAIR.typeSearchLocation
+                            {
+                               Item = new SVCuapiAIR.Airport
+                               {
+                                   Code = "CPT"
+                               }
+                            }
+                        },
+                        Items = new SVCuapiAIR.typeFlexibleTimeSpec[1]
+                        {
+                            new SVCuapiAIR.typeFlexibleTimeSpec
+                            {
+                                PreferredTime = "2019-10-08",
+                            }
+                        }
+                    }
+                },
+                AirSearchModifiers = new SVCuapiAIR.AirSearchModifiers
+                {
+                    PreferredProviders = new SVCuapiAIR.Provider[1]
+                    {
+                        new SVCuapiAIR.Provider
+                        {
+                            Code = "1G"        //CODE FOR GALLILEO, currently the only one we are using
+                        }
+                    }
+                },
+                SearchPassenger = new SVCuapiAIR.SearchPassenger[2]
+                {
+                    new SVCuapiAIR.SearchPassenger
+                    {
+                        Code = "ADT",
+                        BookingTravelerRef = "asgewertjherth"
+                    },
+                    new SVCuapiAIR.SearchPassenger
+                    {
+                        Code = "ADT",
+                        BookingTravelerRef = "tyjrtyrtertyrety"
+                    }
+                }
+            });
+            client.Close();
+        }
+    }
+}
